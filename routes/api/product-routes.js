19fde8e1bc631da6503451d36b4fdfd9f8690778
Andres <garelan@gmail.com>
2022-05-02 22:58:48 -0500

    added find all and find by id api routes for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [{ model: Category, 
+      attributes: ['category_name'],
+      },
+      {
+        model: Tag, through: ProductTag,
+        attributes: ['tag_name'],
+      }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [{ model: Category,
+        attributes: ['category_name'],
+      },
+      {model: Tag, through: ProductTag,
+      attributes: ['tag_name'],
+    }],  
+  });
+  res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
